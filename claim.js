ba0742284c772ccbcf5869c221a43e0f06d65f15
d4rk <ciml@protonmail.com>
2023-03-22 13:10:27 +0200

    fix

--- claim.js
+++ claim.js
@@ -20,7 +20,7 @@ while (true) {
             
                 const tx = {
                     'from': wallet,
-                    'gas': process.env.GAS_LIMIT,
+                    'gas': await data.estimateGas({ from: wallet }),
                     'maxFeePerGas': w3.utils.toWei(process.env.GAS_PRICE, 'Gwei'),
                     'maxPriorityFeePerGas': w3.utils.toWei(process.env.PRIORITY_GAS_PRICE, 'Gwei'),
                     'chainId': await w3.eth.getChainId(),
